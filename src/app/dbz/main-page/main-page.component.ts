4cd82c3c816c99149b3bab91447640fcd2bf2607
Daniel Moya <dmalvarado94@gmail.com>
2022-05-13 21:11:27 -0400

    Agregamos Small e interacción button

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Personaje {
+  nombre: string;
+  poder: number;
+
+}
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.css']
+ 
 })
-export class MainPageComponent implements OnInit {
+export class MainPageComponent {
 
-  constructor() { }
+  nuevo: Personaje = {
+    nombre: 'Trunks',
+    poder: 14000
 
-  ngOnInit(): void {
   }
+
+ agregar() {
+
+  console.log('Esta es una prueba')
+
+ }
 
 }
